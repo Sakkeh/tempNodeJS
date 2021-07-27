7ca7c98489722935ad7afb3ec2e4762dfaf3249b
Sakkeh <sakkeh@gmail.com>
2021-07-27 23:44:45 +0300

    changed dir name

--- 12-http.js
+++ 12-http.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
-const server = http.createServer((req,res)=>{
-    console.log(req);
+const server = http.createServer((req, res) => {
+    //console.log(req);
     if (req.url === '/') {
         res.end('Welcome to our home page');
     }
